0a523a2028d6f749ac785c56df3e337e3a3f90db
forex911 <207845669+forex911@users.noreply.github.com>
2026-03-15 15:57:11 +0530

    Fix next.config.js syntax

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
       },
+    ],
   },
-  reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
